1d81a6f914b7bda1286149f954206925ebdd9554
Miraj Shaikh <shaikhmiraj604@gmail.com>
2024-01-08 20:05:31 +0530

    billdetails data class added

---
+++ app/src/main/java/com/example/chainwala/modal/BillDetails.kt
@@ -0,0 +1,15 @@
+package com.example.chainwala.modal
+
+data class BillDetails(
+
+    val itemName:String,
+    val itemWeight:String,
+    val meltPercent:Double,
+    val wastagePercent:Double,
+    val meltPlusWastage:Double,
+    val fine:Double,
+    val _9950:Double,
+    val tunch:Double,
+    val amount:Double
+
+)
