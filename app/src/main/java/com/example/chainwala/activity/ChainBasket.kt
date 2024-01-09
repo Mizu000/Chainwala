9ef9b06d17669fa46153343caf1035ee9c1db7d2
Miraj Shaikh <shaikhmiraj604@gmail.com>
2024-01-09 18:40:03 +0530

    chain basket completed

--- app/src/main/java/com/example/chainwala/activity/ChainBasket.kt
+++ app/src/main/java/com/example/chainwala/activity/ChainBasket.kt
@@ -2,11 +2,30 @@ package com.example.chainwala.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.chainwala.R
+import com.example.chainwala.adapter.ItemListBillAdapter
 
 class ChainBasket : AppCompatActivity() {
+
+    //recycler varibale
+    lateinit var RcvLayoutManager: RecyclerView.LayoutManager
+    lateinit var rcvAdapter:ItemListBillAdapter
+    lateinit var rcvView:RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chain_basket)
+
+        //recyoler initialize
+        rcvView = findViewById(R.id.rcvBillDetails)
+        rcvAdapter = ItemListBillAdapter(MainActivity.arrayPerItemBill)
+        RcvLayoutManager = LinearLayoutManager(this@ChainBasket, LinearLayoutManager.VERTICAL, false)
+        rcvView.adapter = rcvAdapter
+        rcvView.layoutManager = RcvLayoutManager
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/chainwala/adapter/ItemListBillAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.chainwala.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chainwala.R
+import com.example.chainwala.modal.BillDetails
+
+class ItemListBillAdapter(val perItemBill:ArrayList<BillDetails>):
+    RecyclerView.Adapter<ItemListBillAdapter.ItemListBillHolder>() {
+
+
+    class ItemListBillHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+        val itemName:TextView = itemView.findViewById(R.id.txtItemDesc)
+        val itemWeight:TextView = itemView.findViewById(R.id.txtItemWeight)
+        val itemMelt:TextView = itemView.findViewById(R.id.txtItemMelt)
+        val itemWastage:TextView = itemView.findViewById(R.id.txtItemWastage)
+        val meltPlusWastage:TextView = itemView.findViewById(R.id.txtItemMeltPlusWastage)
+        val fineGold:TextView = itemView.findViewById(R.id.txtItemFineGold)
+        val _9950Gold:TextView = itemView.findViewById(R.id.txtItem9950Gold)
+        val tunchGold:TextView = itemView.findViewById(R.id.txtItemTucnhGold)
+        val amountToPay:TextView = itemView.findViewById(R.id.txtItemPayAmount)
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemListBillHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.per_item_bill_layout, parent, false)
+        return ItemListBillHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+
+        return perItemBill.size
+    }
+
+    override fun onBindViewHolder(holder: ItemListBillHolder, position: Int) {
+
+        val fullBill = perItemBill[position]
+        //
+        holder.itemName.text = "${position+1}. ${fullBill.itemName}"
+        holder.itemWeight.text = holder.itemWeight.toString()
+        holder.itemMelt.text = fullBill.meltPercent.toString()
+        holder.itemWastage.text = fullBill.wastagePercent.toString()
+        holder.meltPlusWastage.text = fullBill.meltPlusWastage.toString()
+        holder.fineGold.text = fullBill.fine.toString()
+        holder._9950Gold.text = fullBill._9950.toString()
+        holder.tunchGold.text = fullBill.tunch.toString()
+        holder.amountToPay.text = fullBill.amount.toString()
+
+    }
+
+}
